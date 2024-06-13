b9d12e6b3c412b75689a771b45f2d8048f4743a2
dor.samet <dori.samet@gmail.com>
2024-06-13 09:59:46 +0300

    Initial commit

---
+++ app/src/main/java/com/engage/engageexampleapplication/MainActivity.kt
@@ -0,0 +1,75 @@
+package com.engage.engageexampleapplication
+
+import android.os.Bundle
+import android.widget.FrameLayout
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.ui.PlayerView
+import com.engage.emadsdk.*
+import com.engage.engageexampleapplication.ui.theme.EngageExampleApplicationTheme
+
+@UnstableApi
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            EngageExampleApplicationTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    AndroidView(factory = { context ->
+                        FrameLayout(context).apply {
+                            layoutParams = FrameLayout.LayoutParams(
+                                FrameLayout.LayoutParams.MATCH_PARENT,
+                                FrameLayout.LayoutParams.MATCH_PARENT
+                            )
+                            val playerView = PlayerView(context).apply {
+                                layoutParams = FrameLayout.LayoutParams(
+                                    FrameLayout.LayoutParams.MATCH_PARENT,
+                                    FrameLayout.LayoutParams.MATCH_PARENT
+                                )
+                            }
+                            playerView.player = ExoPlayer.Builder(context)
+                                .build()
+                            addView(playerView)
+                            addView(EMAdView(context).apply {
+                                layoutParams = FrameLayout.LayoutParams(
+                                    FrameLayout.LayoutParams.MATCH_PARENT,
+                                    FrameLayout.LayoutParams.MATCH_PARENT
+                                )
+                                setAdEventListener(DefaultVideoPlayerListener(playerView))
+                            })
+                        }
+
+                    }, modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun Greeting(name: String, modifier: Modifier = Modifier) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun GreetingPreview() {
+        EngageExampleApplicationTheme {
+            Greeting("Android")
+        }
+    }
+}
